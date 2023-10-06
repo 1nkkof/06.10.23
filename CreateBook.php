ef751bfbebd11c67ef2aecf70de048d7f2276749
1nkkof <144583305+1nkkof@users.noreply.github.com>
2023-10-06 15:47:52 +0400

    Add files via upload

---
+++ CreateBook.php
@@ -0,0 +1,12 @@
+ <?php
+    
+//     require_once './vendor/connect.php';
+//      $author = $_POST['name'];
+//     $name = $_POST['name'];
+//     $imgBook = $_POST['img'];
+//  $description = $_POST['description'];
+ 
+
+//  mysqli_query($connect, "INSERT INTO `Recommendations` (`nameBook1`, `nameAutor1`,`img`,`description`) VALUES ('$name', '$author',$imgBook,'$description')");
+//  header('Location: ./adminPage.php');
+ ?>
---
+++ footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/guestPage.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <title>BookTree</title>
+</head>
+
+<body>
+        <footer class="footer">
+            <div class="container">
+                <div class="container-footer">
+                    <div class="container-footer__images">
+                        <a href="guestPage.php" class="nav-menu__logo logo-footer"><img src="images/logo-light.svg" alt="logo" class="logo-header"></a>
+                    </div>
+                    <div class="container-footer__link">
+                        <a href="" class="link-footer">Каталог</a>
+                        <a href="" class="link-footer">О нас</a>
+                        <a href="" class="link-footer">Вход</a>
+                    </div>
+                    <div class="container-footer__link">
+                        <a href="" class="link-footer">Художественная литература</a>
+                        <a href="" class="link-footer">Научпоп</a>
+                        <a href="" class="link-footer">Букинистика</a>
+                    </div>
+                    <div class="container-telegram">
+                        <div class="container-telegram__link">
+                            <img src="/images/telegram.svg" alt="" class="img-telegram">
+                            <a href="#" class="text-telegram">@Hartunderblade</a>
+                        </div>
+                        <div class="container-telegram__link">
+                            <img src="/images/telegram.svg" alt="" class="img-telegram">
+                            <a href="#" class="text-telegram">@inkk_fake</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+</body>
+
+</html>
